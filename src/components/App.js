f2bf780391505d25e662c51f04b23cda93115d3d
Weronika Tomaszewska <veratomaszewska@gmail.com>
2020-03-24 15:36:18 -0400

    feat: add redux, react-redux, react-router dom; add Nav, MainPage, NewQuestion, QuestionPage and ScoreBoard components and their routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+import Nav from './Nav';
+import MainPage from './MainPage';
+import QuestionPage from './QuestionPage';
+import NewQuestion from './NewQuestion';
+import ScoreBoard from './ScoreBoard';
 
 function App() {
   return (
-    <div className="App">
-      Would You Rather App
-    </div>
+    <Router>
+      <Nav />
+      <Route path="/" exact component={MainPage} />
+      <Route path="/questions/:id" component={QuestionPage} />
+      <Route path="/new" component={NewQuestion} />
+      <Route path="/scoreboard" component={ScoreBoard} />
+    </Router>
   );
 }
 
---
+++ src/components/QuestionPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class QuestionPage extends Component {
+    render() {
+        return (
+            <div>
+                Question Page
+            </div>
+        )
+    }
+}
