17d925ea9c846a251b89c6425f002c0431ad87f7
Weronika Tomaszewska <vikariusu@gmail.com>
2020-03-25 14:26:12 -0400

    feat: fetch initial data (users, questions and authUser) in the App component, add logger.js as middleware (#2)

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { connect } from 'react-redux'
+import getInitialData from '../actions/shared';
 
 import Nav from './Nav';
 import MainPage from './MainPage';
@@ -7,16 +9,23 @@ import QuestionPage from './QuestionPage';
 import NewQuestion from './NewQuestion';
 import ScoreBoard from './ScoreBoard';
 
-function App() {
-  return (
-    <Router>
-      <Nav />
-      <Route path="/" exact component={MainPage} />
-      <Route path="/questions/:id" component={QuestionPage} />
-      <Route path="/new" component={NewQuestion} />
-      <Route path="/scoreboard" component={ScoreBoard} />
-    </Router>
-  );
+
+class App extends React.Component {
+  componentDidMount() {
+    this.props.dispatch(getInitialData())
+  }
+
+  render() {
+    return (
+      <Router>
+        <Nav />
+        <Route path="/" exact component={MainPage} />
+        <Route path="/questions/:id" component={QuestionPage} />
+        <Route path="/new" component={NewQuestion} />
+        <Route path="/scoreboard" component={ScoreBoard} />
+      </Router>
+    );
+  }
 }
 
-export default App;
+export default connect()(App);
