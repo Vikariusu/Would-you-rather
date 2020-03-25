c9dc0f57c577760c932ce10cc5f13c79a1cf1cf2
Weronika Tomaszewska <veratomaszewska@gmail.com>
2020-03-25 17:00:53 -0400

    feat: connect MainPage to Redux, display unanswered and answered questions, redirect to question page on click

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,11 +1,65 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class MainPage extends Component {
+class MainPage extends Component {
+    toQuestion = (e, id) => {
+        e.preventDefault();
+        this.props.history.push(`/questions/${id}`)
+    }
+
     render() {
         return (
             <div>
-                Main Page
+                <h2>Unaswered</h2>
+                <ul>
+                    {this.props.unansweredQuestions.map(question => (
+                        <li>
+                            <div>Would you rather...</div>
+                            <div>
+                                <span>{question.optionOne.text}</span> OR <span>{question.optionTwo.text}</span>
+                            </div>
+                            <button onClick={this.toQuestion}>Vote</button>
+                        </li>
+                    ))}
+                </ul>
+
+                <h2>Answered</h2>
+                <ul>
+                    {this.props.answeredQuestions.map(question => (
+                        <li>
+                            <div>Would you rather...</div>
+                            <div>
+                                <span>{question.optionOne.text}</span> OR <span>{question.optionTwo.text}</span>
+                            </div>
+                            <button onClick={(e) => this.toQuestion(e, question.id)}>Vote</button>
+                        </li>
+                    ))}
+                </ul>
+
             </div>
+
+
         )
     }
 }
+
+function sortByTimestamp(questions) {
+    return questions.sort((a, b) => b.timestamp - a.timestamp)
+}
+
+function mapStateToProp({ questions, users, authedUser }) {
+    const answeredQuestionsIds = users[authedUser]
+        ? Object.keys(users[authedUser].answers)
+        : [];
+    const unansweredQuestionIds = Object.keys(questions).filter(questionId => !answeredQuestionsIds.includes(questionId))
+
+    const answeredQuestions = answeredQuestionsIds.map(questionId => questions[questionId])
+    const unansweredQuestions = unansweredQuestionIds.map(questionId => questions[questionId])
+
+    return {
+        answeredQuestions: sortByTimestamp(answeredQuestions),
+        unansweredQuestions: sortByTimestamp(unansweredQuestions)
+    }
+}
+
+export default connect(mapStateToProp)(MainPage);
