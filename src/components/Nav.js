f154a078d19da4b13eb8f1d475ba92a3471f64b0
Weronika Tomaszewska <vikariusu@gmail.com>
2020-03-27 12:15:49 -0400

    feat: display Nav links - Homepage, Scoreboard, Add question, display user greeting and avatar if logged in, add login/logout functionality, connect Nav component to redux (#5)

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,51 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { setAuthedUser } from '../actions/authedUser'
 
-export default class Nav extends Component {
+class Nav extends Component {
+    toggleAuthedUser = () => {
+        const currentAuthedUser = 'sarahedo'; // temporary authed user
+
+        if (!this.props.authedUser) {
+            this.props.dispatch(setAuthedUser(currentAuthedUser))
+        } else {
+            this.props.dispatch(setAuthedUser(null))
+        }
+    }
+
     render() {
+        const { authedUser, user } = this.props;
+        const firstName = user && user.name.split(' ')[0].toString();
+
         return (
-            <div>
-                Navbar
-            </div>
+            <nav>
+                <div className="nav-left">
+                    <Link to="/">Homepage</Link>
+                    <Link to="/scoreboard">Scoreboard</Link>
+                </div>
+                <div className="nav-right">
+                    <Link to="/new">Add question</Link>
+                    {authedUser && (
+                        <div>
+                            <span>Hello {firstName}</span>
+                            <img src={user && user.avatarURL} style={{ width: '30px' }} />
+                        </div>
+                    )}
+                    <button onClick={this.toggleAuthedUser}>
+                        {authedUser ? 'Logout' : 'Login'}
+                    </button>
+                </div>
+            </nav>
         )
     }
 }
+
+function mapStateToProps({ authedUser, users }) {
+    return {
+        authedUser,
+        user: users[authedUser]
+    }
+}
+
+export default connect(mapStateToProps)(Nav);
