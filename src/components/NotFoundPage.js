5908bec600d550e08b42895ac4ed100f6c450c90
Weronika Tomaszewska <vikariusu@gmail.com>
2020-04-03 15:43:40 -0400

    feat: add 404 page styling (#15)

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -1,11 +1,23 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const StyledNotFound = styled.div`
+    text-align: center;
+    margin-top: 12vh;
+    color: #393E41;
+
+    h1 {
+        letter-spacing: 1px;
+    }
+`
 
 export default class NotFoundPage extends Component {
     render() {
         return (
-            <div>
-                This page doesn't exist!
-            </div>
+            <StyledNotFound>
+                <h1>404</h1>
+                <p>This page doesn't exist!</p>
+            </StyledNotFound>
         )
     }
 }
